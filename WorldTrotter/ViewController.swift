71fcbdedfb80b6a635c2b99b81658c003853033c
Randall Mardus <randalljmardus@gmail.com>
2016-04-19 15:47:01 -0400

    setup VC UI

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,17 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blueColor()
-        view.addSubview(firstView)
-    
-        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.greenColor()
-        firstView.addSubview(secondView)
-    }
+   
 
 }
